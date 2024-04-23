68bc1d89579cddb8af8f46a556c3a332099e80e9
Billy Hale <billy.hale@gce.com>
2024-04-23 08:26:06 -0700

    Resolve issue with comma delimiting in override variables. Add output table for debug purposes.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import core from '@actions/core';
 import mergeVariables from './src/mergeVariables.js';
+import { markdownTable } from 'markdown-table';
 
 const defaultVariables = core.getInput('default_variables');
 const dynamicVariables = core.getInput('dynamic_variables') ?? '';
-const overrideVariables = core.getInput('override_variables').replace(",", "\n") ?? ''; //Convert comma delimited to new line
+const overrideVariables = core.getInput('override_variables').replaceAll(",", "\n") ?? ''; //Convert comma delimited to new line
 
 const merged = mergeVariables(defaultVariables, overrideVariables, dynamicVariables)
 
 core.setOutput('merged_variables', merged);
+
+const table = markdownTable(
+  [
+    ['Variable', 'Value'],
+    ...merged.split('\n').map((line) => line.split('='))
+  ]
+);
+
+console.log(table);
